098029f6f94ba7ad4a0f7b3548fc2e6c3cff1f3b
Mykola Kuievda <dankraig23@gmail.com>
2024-08-06 02:35:28 +0100

    Add Q search functionality accessible across all pages

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 
 from .models import Product, Brand, Category, Theme, Season
 
@@ -12,12 +14,32 @@ def catalog(request):
     seasons = Season.objects.all()
     products = Product.objects.all()
 
+    search = None
+
+    if request.GET:
+        if 'search' in request.GET:
+            search = request.GET['search']
+            if not search:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('catalog'))
+
+            queries = (
+                Q(name__icontains=search) |
+                Q(promo_text__icontains=search) |
+                Q(color__icontains=search) |
+                Q(material__icontains=search) |
+                Q(other_details__icontains=search)
+            )
+            products = products.filter(queries)
+
     context = {
         'brands': brands,
         'categories': categories,
         'themes': themes,
         'seasons': seasons,
         'products': products,
+        'search_status': search,
     }
 
     return render(request, 'products/catalog.html', context)
