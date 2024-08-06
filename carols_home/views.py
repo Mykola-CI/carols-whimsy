4c60f1333499305e81da3a87e035d6bba7ad69a2
Mykola Kuievda <dankraig23@gmail.com>
2024-08-06 01:17:21 +0100

    Update nav lists of links from static to dynamic content

--- carols_home/views.py
+++ carols_home/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from products.models import Product, Brand, Category, Theme, Season
 
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'carols_home/index.html')
+    brands = Brand.objects.all()
+    categories = Category.objects.all()
+    themes = Theme.objects.all()
+    seasons = Season.objects.all()
+    products = Product.objects.all()
+
+    context = {
+        'brands': brands,
+        'categories': categories,
+        'themes': themes,
+        'seasons': seasons,
+        'products': products,
+    }
+
+    return render(request, 'carols_home/index.html', context)
--- products/views.py
+++ products/views.py
@@ -6,6 +6,18 @@ from .models import Product, Brand, Category, Theme, Season
 def catalog(request):
     """ A view to return the catalog page """
 
+    brands = Brand.objects.all()
+    categories = Category.objects.all()
+    themes = Theme.objects.all()
+    seasons = Season.objects.all()
     products = Product.objects.all()
 
-    return render(request, 'products/catalog.html', {'products': products})
+    context = {
+        'brands': brands,
+        'categories': categories,
+        'themes': themes,
+        'seasons': seasons,
+        'products': products,
+    }
+
+    return render(request, 'products/catalog.html', context)
