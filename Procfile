a78637061b52d318d29f04652f23a70724ac610d
Mykola Kuievda <dankraig23@gmail.com>
2024-08-12 18:54:31 +0100

    Connect CI db, migrated, populated, apply settings for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn carols_project.wsgi:application
--- products/management/commands/import_excel.py
+++ products/management/commands/import_excel.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from django.core.management.base import BaseCommand
+
 from products.models import Product, Category, Theme, Season, Brand
 
 
