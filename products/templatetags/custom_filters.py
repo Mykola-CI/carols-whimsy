1420c12a3643b329c6356104ea86cc3b7037236d
Mykola Kuievda <dankraig23@gmail.com>
2024-08-14 15:58:33 +0100

    Add custom filter for checking image exist, add styles to add to cart buttons catalog

--- products/templatetags/custom_filters.py
+++ products/templatetags/custom_filters.py
@@ -1,4 +1,6 @@
+import os
 from django import template
+from django.conf import settings
 
 
 register = template.Library()
@@ -17,3 +19,11 @@ def get_quantity(cart_items, product_id):
     product_id_str = str(product_id)
     # Retrieve the quantity from cart_items
     return cart_items.get(product_id_str, 0)
+
+
+@register.filter(name='image_exists')
+def image_exists(image_field):
+    if not image_field or not hasattr(image_field, 'name') or not image_field.name:
+        return False
+    full_path = os.path.join(settings.MEDIA_ROOT, image_field.name)
+    return os.path.isfile(full_path)
