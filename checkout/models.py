b5f58d23a8b83adfbd1c427d0e3616cdd149b422
Mykola Kuievda <dankraig23@gmail.com>
2024-08-15 23:36:29 +0100

    Add post save signal to update order totals

--- checkout/models.py
+++ checkout/models.py
@@ -41,9 +41,13 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('item_total'))['item_total__sum']
+        self.order_total = self.lineitems.aggregate(
+            Sum('item_total')
+        )['item_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = (
+                self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            )
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
--- products/templatetags/custom_filters.py
+++ products/templatetags/custom_filters.py
@@ -1,6 +1,4 @@
-import os
 from django import template
-from django.conf import settings
 
 
 register = template.Library()
@@ -11,19 +9,3 @@ def nan_to_empty(value):
     if value == 'nan':
         return ''
     return value
-
-
-@register.filter
-def get_quantity(cart_items, product_id):
-    # Convert product_id to string to match the keys in cart_items
-    product_id_str = str(product_id)
-    # Retrieve the quantity from cart_items
-    return cart_items.get(product_id_str, 0)
-
-
-@register.filter(name='image_exists')
-def image_exists(image_field):
-    if not image_field or not hasattr(image_field, 'name') or not image_field.name:
-        return False
-    full_path = os.path.join(settings.MEDIA_ROOT, image_field.name)
-    return os.path.isfile(full_path)
