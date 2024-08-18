6e4e3f99be3294ca29b3ac083604244e2e4184c6
Mykola Kuievda <dankraig23@gmail.com>
2024-08-18 18:48:01 +0100

    Add script to template to pass pk and client secret, create JS file and script for Stripe integration

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,25 +1,10 @@
-// const stripe_public_key = document.getElementById('id_stripe_public_key').innerText.slice(1, -1);
-// const clientSecret = document.getElementById('id_client_secret').innerText.slice(1, -1);
-
-// const stripe = Stripe(stripe_public_key);
-
-// const appearance = {
-//     theme: 'flat',
-//     variables: { colorPrimaryText: '#262626' }
-// };
-// const options = {
-//     layout: {
-//         type: 'tabs',
-//         defaultCollapsed: false,
-//     }
-// };
-// const elements = stripe.elements({ clientSecret, appearance });
-// const paymentElement = elements.create('payment', options);
-// paymentElement.mount('#payment-element');
-
-$(document).ready(function() {
+$(document).ready(function () {
     const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
     const clientSecret = $('#id_client_secret').text().slice(1, -1);
+
+    console.log('Client Secret: ' + clientSecret);
+    console.log('Stripe Public Key: ' + stripe_public_key);
+    console.log('Shipping Details: ', shippingDetails);
 
     const stripe = Stripe(stripe_public_key);
 
@@ -36,4 +21,64 @@ $(document).ready(function() {
     const elements = stripe.elements({ clientSecret, appearance });
     const paymentElement = elements.create('payment', options);
     paymentElement.mount('#payment-element');
+
+    // Handle realtime validation errors on the payment element
+    paymentElement.addEventListener('change', function (event) {
+        var errorDiv = document.getElementById('card-errors');
+        if (event.error) {
+            var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+            $(errorDiv).html(html);
+        } else {
+            errorDiv.textContent = '';
+        }
+    });
+
+    // Handle form submit
+    var form = document.getElementById('payment-form');
+
+    form.addEventListener('submit', function (ev) {
+        ev.preventDefault();
+        paymentElement.update({ 'disabled': true });
+        $('#complete-order').attr('disabled', true);
+
+        var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+        var postData = {
+            'csrfmiddlewaretoken': csrfToken,
+            'client_secret': clientSecret,
+        };
+        var url = '/checkout/cache_checkout_data/';
+
+        $.post(url, postData).done(function () {
+            stripe.confirmPayment({
+                elements,
+                confirmParams: {
+                    return_url: `${window.location.origin}/checkout/checkout_success/`,
+                    shipping: shippingDetails
+                },
+                redirect: 'if_required'
+                // Return a promise that resolves when the payment is successful
+            }).then(function (result) {
+                if (result.error) {
+                    var errorDiv = document.getElementById('card-errors');
+                    var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+                    $(errorDiv).html(html);
+                    card.update({ 'disabled': false });
+                    $('#submit-button').attr('disabled', false);
+                } else {
+                    if (result.paymentIntent.status === 'succeeded') {
+                        form.submit();
+                    }
+                }
+            });
+        });
+    });
 });
