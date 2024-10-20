d6f2054862d2a53a18df00b1b2e9f2038184c87a
Mykola Kuievda <dankraig23@gmail.com>
2024-10-20 19:57:26 +0100

    Add allowed hosts for new domains

--- carols_project/settings.py
+++ carols_project/settings.py
@@ -23,7 +23,14 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com', '192.168.1.207']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.herokuapp.com',
+    '192.168.1.207',
+    'carolswhimsy.com',
+    'www.carolswhimsy.com'
+    ]
 
 
 # Application definition
